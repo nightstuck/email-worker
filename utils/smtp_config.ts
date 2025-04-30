a7e3efc0c2689cfdda3b0940b6db2e954bac22d0
Nightstuck <luky-404@web.de>
2025-04-30 10:09:25 +0200

    outsourced config

---
+++ utils/smtp_config.ts
@@ -0,0 +1,10 @@
+export const SMTP_options = {
+    pool: true,
+    host: "	mx.freenet.de",
+    port: 587,
+    secure: true, // use TLS
+    auth: {
+        user: "nightstuck@freenet.de",
+        pass: "!",
+    },
+};
